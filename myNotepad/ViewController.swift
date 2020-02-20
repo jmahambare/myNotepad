cb5a576e6be733844fea5a2e054b4b8e74f6d682
Jitendra Mahambare <jmahambare@yahoo.com>
2020-02-20 12:49:57 +0530

    Initial commit

---
+++ myNotepad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myNotepad
+//
+//  Created by jitendra mahambare on 20/02/20.
+//  Copyright © 2020 Jitendra Mahambare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
